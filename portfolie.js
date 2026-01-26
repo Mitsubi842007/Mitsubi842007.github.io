b0b2dde38b1292f3bdd207093aaf6dbe4f695130
Mitsubi <agp.842007@gmail.com>
2026-01-26 21:29:17 +0100

    made a test

--- portfolie.js
+++ portfolie.js
@@ -1,1 +1,6 @@
 console.log("welcome to my portfolio");
+
+// Add click event to toggle the card open/close
+document.querySelector('.card').addEventListener('click', function() {
+    this.classList.toggle('open');
+});
